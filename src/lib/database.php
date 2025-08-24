9ff0beaa127877ae0f154702c77f24dcb39da621
Pratikshasapkal <psapkal993@gmail.com>
2025-08-24 20:07:20 +0530

    srcdt

--- src/lib/database.php
+++ src/lib/database.php
@@ -6,7 +6,7 @@ class Database {
         if (self::$pdo === null) {
             $config = require __DIR__ . '/../../config/config.php';
             $dsn = "mysql:host={$config['host']};dbname={$config['dbname']};charset={$config['charset']}";
-            
+
             try {
                 self::$pdo = new \PDO(
                     $dsn,
